b872ed0d6804eabdd6c8c600a1ad900e8930ea09
kh <127534515+kh5402@users.noreply.github.com>
2023-08-24 22:39:36 +0900

    Update Y_Sche.py

--- Y_Sche.py
+++ Y_Sche.py
@@ -1,13 +1,38 @@
-
+import os
+import re
 from bs4 import BeautifulSoup
 from pyppeteer import launch
-import re
 from xml.etree.ElementTree import Element, SubElement, tostring, ElementTree
 from datetime import datetime, timedelta
 import xml.dom.minidom
+import xml.etree.ElementTree as ET
 import asyncio
+import requests
+
+# 既存のXMLファイルから日付とタイトルの組み合わせを取得
+def get_existing_schedules(file_name):
+    existing_schedules = set()
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+    for item in root.findall(".//item"):
+        date = item.find('pubDate').text
+        title = item.find('title').text
+        existing_schedules.add((date, title))
+    return existing_schedules
+
 
 async def main():
+
+    # Discordのwebhook URLを環境変数から取得
+    webhook_url = os.environ['WEBHOOK_URL']
+
+    # 既存のXMLファイルがあれば、その情報を取得
+    existing_file = 'Y_Sche.xml'
+    existing_schedules = get_existing_schedules(existing_file) if os.path.exists(existing_file) else set()
+
+    # 新規情報を保存するリスト
+    new_schedules = []
+
     
     # 当月から今日から3ヶ月先までのyyyymmを生成
     start_date = datetime.today().replace(day=1)
@@ -48,7 +73,7 @@ async def main():
         print('# スケジュール情報の取得')
         day_schedules = soup.find_all('div', class_='sc--day')
 
-        # スクレイピング
+        # 各スケジュールの情報を取得
         for day_schedule in day_schedules:
             date_tag = day_schedule.find('div', class_='sc--day__hd js-pos a--tx')
             if date_tag is None:
@@ -59,12 +84,33 @@ async def main():
                 title = re.search(r'<!--wovn-src:(.*?)-->', str(link.find('p', class_='m--scone__ttl'))).group(1)
                 url = link['href']
                 schedules.append((date, title, url))
+                
+            # 新規情報の確認
+            if (date, title) not in existing_schedules: 
+                new_schedules.append((date, title, url))
+
+
+
+        
+                
         # 次の月へ        
-        current_date += timedelta(days=31)
-        current_date = current_date.replace(day=1)
+        current_date = (current_date + timedelta(days=31)).replace(day=1)
+        if current_date.day != 1: # 月の最初の日ではない場合
+            current_date = (current_date + timedelta(days=1)).replace(day=1) # 月を1つ進める
+    
+    # 新規情報があれば、Discordへ通知
+    if new_schedules:
+        # Discordへの通知処理
+        pass
+        
+    # 既存のスケジュール情報もリスト形式に変換
+    existing_schedules_list = [(date, title, '') for date, title in existing_schedules]
+
+    # 既存の情報と新規情報を合わせる
+    all_schedules = existing_schedules_list + new_schedules
 
     # 日付の降順にソート
-    schedules.sort(key=lambda x: datetime.strptime(x[0], "%Y/%m/%d"), reverse=True)
+    all_schedules.sort(key=lambda x: datetime.strptime(x[0], "%Y/%m/%d"), reverse=True)
 
     # RSSフィードを生成
     rss = Element("rss", version="2.0")
@@ -72,7 +118,7 @@ async def main():
     SubElement(channel, "title").text = "弓木奈於のスケジュール"
     SubElement(channel, "description").text = ""
     SubElement(channel, "link").text = ""
-    for date, title, url in schedules:
+    for date, title, url in all_schedules:
         item = SubElement(channel, "item")
         SubElement(item, "title").text = title
         SubElement(item, "link").text = url
@@ -80,7 +126,7 @@ async def main():
     xml_str = xml.dom.minidom.parseString(tostring(rss)).toprettyxml(indent="   ")
 
     # ファイルに保存
-    with open('Y_Sche.xml', 'w', encoding='utf-8') as f:
+    with open(existing_file, 'w', encoding='utf-8') as f:
         f.write(xml_str)
 
 # 非同期関数を実行
